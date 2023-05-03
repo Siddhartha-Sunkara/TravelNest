843d3091ab987ee46a49acd5846009267d5e22ae
Siddhartha-Sunkara <siddhartha.sunkara20@st.niituniversity.in>
2023-05-04 00:23:34 +0530

    added gallery

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,6 +11,9 @@ import experienceImg from "../assets/images/experience.png";
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
+import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
+import Testimonials from "../components/Testimonial/Testimonials";
+
 
 const Home = () => {
   return (
@@ -38,23 +41,23 @@ const Home = () => {
               </div>
             </Col>
 
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box">
                 <img src={heroImg} alt="" />
               </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box mt-4">
-                <video src={heroVideo} alt="" controls/>
+                <video src={heroVideo} alt="" controls />
               </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box mt-5">
                 <img src={heroImg02} alt="" />
               </div>
             </Col>
 
-            <SearchBar/>
+            <SearchBar />
           </Row>
         </Container>
       </section>
@@ -62,11 +65,11 @@ const Home = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='3'>
+            <Col lg="3">
               <h5 className="services__subtitle">What we serve</h5>
               <h2 className="services__title">We offer our best services</h2>
             </Col>
-            <ServiceList/>
+            <ServiceList />
           </Row>
         </Container>
       </section>
@@ -75,11 +78,11 @@ const Home = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='12' className="mb-5">
-              <Subtitle subtitle={"Explore"}/>
+            <Col lg="12" className="mb-5">
+              <Subtitle subtitle={"Explore"} />
               <h2 className="featured__tour-title">Our feautred tours</h2>
             </Col>
-            <FeaturedTourList/>
+            <FeaturedTourList />
           </Row>
         </Container>
       </section>
@@ -88,14 +91,19 @@ const Home = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='6'>
+            <Col lg="6">
               <div className="experience__content">
-                <Subtitle subtitle={'Experience'}/>
-                <h2>With our all experience <br /> we will serve you</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa fugiat, cupiditate, corporis cumque quidem culpa eligendi,
-                  <br /> 
-                  facere obcaecati minima repudiandae rerum ad libero odio deleniti fuga. Molestiae in accusamus esse!
-                  </p>
+                <Subtitle subtitle={"Experience"} />
+                <h2>
+                  With our all experience <br /> we will serve you
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+                  fugiat, cupiditate, corporis cumque quidem culpa eligendi,
+                  <br />
+                  facere obcaecati minima repudiandae rerum ad libero odio
+                  deleniti fuga. Molestiae in accusamus esse!
+                </p>
               </div>
 
               <div className="counter__wrapper d-flex aligh-items-center gap-5">
@@ -123,21 +131,40 @@ const Home = () => {
       </section>
       {/* =============== experience section end =============== */}
 
-
       {/* =============== gallery section start =============== */}
       <section>
         <Container>
           <Row>
-            <Col lg='12'>
-              <Subtitle subtitle={'Gallery'}/>
-              <h2 className="gallery_title">
-                Visit our customer tour gallery
-              </h2>
+            <Col lg="12">
+              <Subtitle subtitle={"Gallery"} />
+              <h2 className="gallery_title">Visit our customer tour gallery</h2>
+            </Col>
+            <Col lg="12">
+              <MasonryImagesGallery />
             </Col>
           </Row>
         </Container>
       </section>
       {/* =============== gallery section end =============== */}
+      {/* =============== Testimonial section end =============== */}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <Subtitle subtitle={'Fans Love'}/>
+            <h2 className="testimonial__title">What our users say about us</h2>
+          </Col>
+          <Col lg='12'>
+            <Testimonials />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+
+
+
+      {/* =============== Testimonial section end =============== */}
     </>
   );
 };
