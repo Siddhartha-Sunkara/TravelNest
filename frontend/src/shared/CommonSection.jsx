d9ad48619828d39433cac76b5aecee58e80760a9
adarsh <mishraadarsh530@gmail.com>
2023-05-04 23:31:37 +0530

    Updated tours

--- frontend/src/shared/CommonSection.jsx
+++ frontend/src/shared/CommonSection.jsx
@@ -1,21 +1,19 @@
-
 import React from 'react'
 import './common-section.css'
-import { Container, Row, Col} from 'reactstrap';
+import { Container, Row ,Col } from 'reactstrap'
 
-const CommonSection = ({tittle}) => {
+const CommonSection = ({title}) => {
   return (
-   <section className= "common__section">
-    <Container>
-        <Row>
-        <Col lg='12'>
-        <h1>{tittle}</h1>
+   <section className="common__section">
+   <Container>
+    <Row>
+        <Col lg="12">
+            <h1>{title}</h1>
         </Col>
-        </Row>
-    </Container>
+    </Row>
+   </Container>
    </section>
-
-  );
-};
+  )
+}
 
 export default CommonSection
