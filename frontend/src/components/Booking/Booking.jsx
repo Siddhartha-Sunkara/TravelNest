18a53dc8dcae97d6d763b37e539bc983ba5c239d
Shiv1909 <122529354+Shiv1909@users.noreply.github.com>
2023-05-05 16:08:14 +0530

    Done

---
+++ frontend/src/components/Booking/Booking.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './booking.css'
+import { Form, FormGroup, ListGroup, ListGroupItem, Button } from 'reactstrap'
+
+
+const Booking = ({ tour, avgRating }) => {
+
+    const {price, reviews} = tour 
+
+    const handleChange = e => {};
+
+  return (
+    <div className='booking'>
+      <div className="booking__top d-flex align-itmes-center justify-content-between">
+        <h3>${price} <span>/per person/</span> </h3>
+        <span className='tour__rating d-flex align-iteam-center'>
+            <i class="ri-star-fill"  ></i>
+            {avgRating === 0 ? null : avgRating} ({reviews?.length})        
+        </span>
+      </div>
+      {/*  ========== booking form ==============*/}
+      <div className="booking__form">
+        <h5>Information</h5>
+        <Form className='booking__info-form'>
+          <FormGroup>
+            <input type="text" placeholder='Full Name' id='fullname' required  onChange={handleChange}/>
+          </FormGroup>         
+          <FormGroup>
+            <input type="number" placeholder='Phone' id='phone' required  onChange={handleChange}/>
+          </FormGroup>         
+          <FormGroup className="d-flex align-items-center gap-3">
+            <input type="date" placeholder='' id='bookAt' required  onChange={handleChange}/>
+            <input type="number" placeholder='Guest' id='guestSize' required  onChange={handleChange}/>
+          </FormGroup>
+        </Form>
+      </div>
+      {/*  ========== booking end ==============*/}
+    </div>
+  )
+}
+
+export default Booking
