2244a2e9c61ca5a86d699a874a8a019c73a82567
Siddhartha-Sunkara <100306435+Siddhartha-Sunkara@users.noreply.github.com>
2023-05-10 00:13:47 +0530

    Backend 2 (#22)
    * changes made
    
    * tour changes

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -6,7 +6,7 @@ import calculateAvgRating from '../utils/avgRating';
 
 const TourCard = ({tour}) => {
 
-  const {id, title, city, photo, price, featured, reviews } = tour
+  const {_id, title, city, photo, price, featured, reviews } = tour
   
   const {totalRating, avgRating} = calculateAvgRating(reviews)
 
@@ -19,9 +19,9 @@ const TourCard = ({tour}) => {
         <CardBody>
             <div className='card__top d-flex align-items-center justify-content-between'>
                 <span className='tour__localtion d-flex align-items-center gap-1'>
-                    <i class="ri-map-pin-line"></i> {city}
+                    <i className="ri-map-pin-line"></i> {city}
                 </span>\<span className='tour__rating d-flex align-items-center gap-1'>
-                    <i class="ri-star-fill"></i> {avgRating === 0 ? null : avgRating}
+                    <i className="ri-star-fill"></i> {avgRating === 0 ? null : avgRating}
                     {totalRating === 0 ? ( 
                         'Not rated'
                         ) : ( <span>({reviews.length})</span> 
@@ -30,13 +30,13 @@ const TourCard = ({tour}) => {
                 </span>
             </div>
 
-            <h5 className="tour__tit"><Link to={`/tours/${id}`}>{title}</Link></h5>
+            <h5 className="tour__title"><Link to={`/tours/${_id}`}>{title}</Link></h5>
 
             <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
                 <h5>${price} <span> /per person</span></h5>
 
                 <button className='btn booking__btn'>
-                    <Link to={`/tours/${id}`}>Book Now</Link>
+                    <Link to={`/tours/${_id}`}>Book Now</Link>
                 </button>
             </div>
         </CardBody>
